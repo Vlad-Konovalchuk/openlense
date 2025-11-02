0a5dc7cd5a56fc9c2500afbf350d697e9d752533
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2025-11-02 18:24:55 +0200

    fix: fixed layout component

--- apps/frontend/src/layouts/MainLayout.tsx
+++ apps/frontend/src/layouts/MainLayout.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Header from '../components/Header';
 
 const MainLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
-	<div className='min-h-screen bg-gray-50'>
-		<Header />
-		<main className='max-w-6xl mx-auto p-6'>{children}</main>
-	</div>
+  <div className='min-h-screen bg-gray-50'>
+    <Header />
+    <main className='max-w-6xl mx-auto p-6'>{children}</main>
+  </div>
 );
 
 export default MainLayout;
